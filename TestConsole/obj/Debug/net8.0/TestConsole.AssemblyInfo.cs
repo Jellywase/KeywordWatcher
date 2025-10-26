1f65ef5e1d4c1358f8259f38770b780c3b88f652
unknown <jellywase@naver.com>
2025-10-26 16:55:00 +0900

    d

--- TestConsole/obj/Debug/net8.0/TestConsole.AssemblyInfo.cs
+++ TestConsole/obj/Debug/net8.0/TestConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05ab22e7fbbb1800b0653c4e83c8e987a7bf47e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af5bfce113046cff5f15f0d1c26ccd3d65f1bc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
